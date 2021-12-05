46dc596577b5bc05ac51a83f6259f9b4a576ef68
t0paz-2357 <90430489+t0paz-2357@users.noreply.github.com>
2021-12-05 19:30:04 +0900

    Create 짝수와 홀수.py

---
+++ Level 1/짝수와 홀수.py
@@ -0,0 +1,15 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12937
+
+-----Mine-----
+def solution(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+-----Others-----
+def evenOrOdd(num):
+    if (num%2):
+        return "Odd"
+    else:
+        return "Even"

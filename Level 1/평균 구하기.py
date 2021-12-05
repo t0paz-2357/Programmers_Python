c90803863fffcabc227d66f70f4d6e1f5de8e85e
t0paz-2357 <90430489+t0paz-2357@users.noreply.github.com>
2021-12-05 17:43:24 +0900

    Create 평균 구하기.py

---
+++ Level 1/평균 구하기.py
@@ -0,0 +1,9 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12944
+
+-----Mine-----
+def solution(arr):
+    return sum(arr) / len(arr)
+
+-----Others-----
+def average(list):
+    return (sum(list) / len(list))

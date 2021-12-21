68c95db6174a3e4bc72544f7a049bf4f2db7c703
t0paz-2357 <90430489+t0paz-2357@users.noreply.github.com>
2021-12-21 23:07:19 +0900

    Create 문자열을 정수로 바꾸기.py

---
+++ Level 1/문자열을 정수로 바꾸기.py
@@ -0,0 +1,10 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12925
+
+-----Mine-----
+def solution(s):
+    return int(s)
+
+-----Others-----
+def strToInt(str):
+    a = int(str)
+    return a

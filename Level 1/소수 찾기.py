1529935f4960e2b8c4ce1b32e9b32712cae54a1a
t0paz-2357 <90430489+t0paz-2357@users.noreply.github.com>
2021-12-30 18:07:26 +0900

    Create 소수 찾기.py

---
+++ Level 1/소수 찾기.py
@@ -0,0 +1,33 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12921
+
+-----Mine-----
+import math
+
+def isPrime(num):
+    if num == 2:
+        return True
+
+    for i in range(2, int(math.sqrt(num)+1)):
+        if num % i == 0:
+            return False
+
+    return True
+
+def solution(n):
+    answer = 0
+
+    for i in range(2, n + 1):
+        if isPrime(i) == True:
+            answer += 1
+
+    return answer 
+
+
+-----Others-----
+def solution(n):
+    num=set(range(2,n+1)) # 2부터 n+1까지의 집합
+
+    for i in range(2,n+1): # 2부터 n까지 반복문
+        if i in num: # 만약 i가 num 집합에 있다면
+            num-=set(range(2*i,n+1,i)) # i의 배수는 num 집합에서 제외
+    return len(num) # num에 남아있는 숫자의 개수가 소수의 개수

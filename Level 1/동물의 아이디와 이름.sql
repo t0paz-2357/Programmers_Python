bc4e612dd82586057c1706feae6c3e2051ce92e3
t0paz-2357 <90430489+t0paz-2357@users.noreply.github.com>
2021-12-30 23:53:00 +0900

    Create 동물의 아이디와 이름.sql

---
+++ Level 1/동물의 아이디와 이름.sql
@@ -0,0 +1,7 @@
+# https://programmers.co.kr/learn/courses/30/lessons/59403
+
+-----MySQL-----
+SELECT ANIMAL_ID, NAME FROM ANIMAL_INS ORDER BY ANIMAL_ID
+
+-----Oracle-----
+SELECT ANIMAL_ID, NAME FROM ANIMAL_INS ORDER BY ANIMAL_ID

a9d266f6b775e16d8241bd60eed8eadcfca60360
t0paz-2357 <90430489+t0paz-2357@users.noreply.github.com>
2021-12-30 18:16:59 +0900

    Create 서울에서 김서방 찾기.py

---
+++ Level 1/서울에서 김서방 찾기.py
@@ -0,0 +1,9 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12919
+
+-----Mine-----
+def solution(seoul):
+    return "김서방은 " + str(seoul.index("Kim")) + "에 있다"
+
+-----Others-----
+def findKim(seoul):
+    return "김서방은 {}에 있다".format(seoul.index('Kim'))

7744b3cd6621d8bd62dd7d1fdcbc28c0df4b2738
t0paz-2357 <90430489+t0paz-2357@users.noreply.github.com>
2022-01-01 16:58:48 +0900

    Create 모든 레코드 조회하기.sql

---
+++ Level 1/모든 레코드 조회하기.sql
@@ -0,0 +1,7 @@
+# https://programmers.co.kr/learn/courses/30/lessons/59034
+
+-----MySQL-----
+SELECT * FROM ANIMAL_INS ORDER BY ANIMAL_ID
+
+-----Oracle-----
+SELECT * FROM ANIMAL_INS ORDER BY ANIMAL_ID
